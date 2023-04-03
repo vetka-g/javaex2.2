80cf4fb41e2fa53354e5718526829e6d80e7db7f
Sveta Golubeva <veta.golubeva@gmail.com>
2023-04-03 16:45:47 +0300

    Main updated

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
+        int currentAmount = 120;
+        int refill = 1500;
+        int bonus = refill >= 1000 ? refill / 100 : 0;
+        int finalScore = currentAmount + refill + bonus;
 
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
+        // Второй вариант
+        // int bonus;
+        // if (refill >= 1000) {
+        // bonus = refill / 100;
+        // } else {
+        // bonus = 0;
+        // }
+
+        System.out.println("Итоговый счет:" + finalScore);
+        System.out.println("Количество бонусов:" + bonus);
     }
+
+
 }
